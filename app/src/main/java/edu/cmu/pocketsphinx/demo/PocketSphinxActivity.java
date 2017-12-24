32c1ece168040b1c8be8fd63adab7cda14244b6b
asarsembayev <dos_lp@mail.ru>
2017-12-24 23:56:30 +0600

    I have removed all unnecessary dictionaries and lms. The program now performs words and cont.speech recognition.

--- app/src/main/java/edu/cmu/pocketsphinx/demo/PocketSphinxActivity.java
+++ app/src/main/java/edu/cmu/pocketsphinx/demo/PocketSphinxActivity.java
@@ -32,18 +32,25 @@ package edu.cmu.pocketsphinx.demo;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 
 import edu.cmu.pocketsphinx.Assets;
@@ -59,11 +66,16 @@ public class PocketSphinxActivity extends Activity implements
 
     /* Named searches allow to quickly reconfigure the decoder */
     private static final String KWS_SEARCH = "wakeup";
-    private static final String FORECAST_SEARCH = "forecast";
-    private static final String DIGITS_SEARCH = "digits";
-    private static final String PHONE_SEARCH = "phones";
     private static final String MENU_SEARCH = "menu";
-    private static final String CONT_SPEECH = "mine";
+    private static final String CONT_SPEECH = "speech";
+    private static final String MINE_GRAM = "words";
+
+    private Button correct_btn;
+    private Button wrong_btn;
+    private Button reset_btn;
+    private Button export_btn;
+    private int correct_answers = 0;
+    private int wrong_answers = 0;
 
     /* Keyword we are looking for to activate menu */
     private static final String KEYPHRASE = "sphinx";
@@ -82,10 +94,8 @@ public class PocketSphinxActivity extends Activity implements
         captions = new HashMap<>();
         captions.put(KWS_SEARCH, R.string.kws_caption);
         captions.put(MENU_SEARCH, R.string.menu_caption);
-        captions.put(DIGITS_SEARCH, R.string.digits_caption);
-        captions.put(PHONE_SEARCH, R.string.phone_caption);
-        captions.put(FORECAST_SEARCH, R.string.forecast_caption);
-        captions.put(CONT_SPEECH, R.string.my_exmpl);
+        captions.put(MINE_GRAM, R.string.words_rec);
+        captions.put(CONT_SPEECH, R.string.cont_speech_rec);
         setContentView(R.layout.main);
         ((TextView) findViewById(R.id.caption_text))
                 .setText("Preparing the recognizer");
@@ -99,6 +109,11 @@ public class PocketSphinxActivity extends Activity implements
         // Recognizer initialization is a time-consuming and it involves IO,
         // so we execute it in async task
         new SetupTask(this).execute();
+
+        correct_btn = (Button) findViewById(R.id.correct);
+        wrong_btn = (Button) findViewById(R.id.wrong);
+        reset_btn = (Button) findViewById(R.id.reset);
+        export_btn = (Button) findViewById(R.id.export_Stats);
     }
 
     private static class SetupTask extends AsyncTask<Void, Void, Exception> {
@@ -167,12 +182,8 @@ public class PocketSphinxActivity extends Activity implements
         String text = hypothesis.getHypstr();
         if (text.equals(KEYPHRASE))
             switchSearch(MENU_SEARCH);
-        else if (text.equals(DIGITS_SEARCH))
-            switchSearch(DIGITS_SEARCH);
-        else if (text.equals(PHONE_SEARCH))
-            switchSearch(PHONE_SEARCH);
-        else if (text.equals(FORECAST_SEARCH))
-            switchSearch(FORECAST_SEARCH);
+        else if (text.equals(MINE_GRAM))
+            switchSearch(MINE_GRAM);
         else if (text.equals(CONT_SPEECH))
             switchSearch(CONT_SPEECH);
         else
@@ -184,6 +195,7 @@ public class PocketSphinxActivity extends Activity implements
      */
     @Override
     public void onResult(Hypothesis hypothesis) {
+
         ((TextView) findViewById(R.id.result_text)).setText("");
         if (hypothesis != null) {
             String text = hypothesis.getHypstr();
@@ -200,8 +212,77 @@ public class PocketSphinxActivity extends Activity implements
      */
     @Override
     public void onEndOfSpeech() {
+        ((TextView) findViewById(R.id.caption_text)).setText("Was the utterance recognized correct or wrong?");
+        correct_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                correct_answers++;
+                ((TextView) findViewById(R.id.caption_text)).setText("Correct, the # is " + correct_answers);
+                if (((TextView) findViewById(R.id.correct_num)).getText() != null){
+                    ((TextView) findViewById(R.id.correct_num)).setText(Integer.toString(correct_answers));
+                }
+            }
+        });
+        wrong_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                wrong_answers++;
+                ((TextView) findViewById(R.id.caption_text)).setText("Wrong, the # is " + wrong_answers);
+                if (((TextView) findViewById(R.id.wrong_num)).getText() != null){
+                    ((TextView) findViewById(R.id.wrong_num)).setText(Integer.toString(wrong_answers));
+                }
+            }
+        });
+        reset_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                wrong_answers = 0;
+                correct_answers = 0;
+                ((TextView) findViewById(R.id.caption_text)).setText("Reset the answers counter to 0");
+            }
+        });
+
+        export_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+                String currentDateandTime = sdf.format(new Date());
+                String fileName = "_stats_" + currentDateandTime + "_.txt";
+                String str = "The stats for: " + currentDateandTime + "\n\n" +
+                        "Correct #: " + correct_answers + "\nWrong #: " + wrong_answers;
+                String alt_path = "Android/data/edu.cmu.sphinx.pocketsphinx/files/stats/";
+
+                Context context = getApplicationContext();
+                File path = context.getFilesDir();
+                File file = new File(path, fileName);
+                System.out.println("the path: "+path + " file name " + file
+                +"\n the STR " +str);
+
+                FileOutputStream stream = null;
+                try {
+                    stream = new FileOutputStream(file);
+                    stream.write(str.getBytes());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        stream.close();
+                        System.out.println("closedDDDDD");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                ((TextView) findViewById(R.id.caption_text)).setText("Export");
+            }
+        });
+
         if (!recognizer.getSearchName().equals(KWS_SEARCH))
             switchSearch(KWS_SEARCH);
+        ((TextView) findViewById(R.id.caption_text)).setText("End of speech");
+
     }
 
     private void switchSearch(String searchName) {
@@ -225,7 +306,7 @@ public class PocketSphinxActivity extends Activity implements
                 .setAcousticModel(new File(assetsDir, "en-us-ptm"))
                 .setDictionary(new File(assetsDir, "cmudict-en-us.dict"))
 
-                .setRawLogDir(assetsDir) // To disable logging of raw audio comment out this call (takes a lot of space on the device)
+                //.setRawLogDir(assetsDir) // To disable logging of raw audio comment out this call (takes a lot of space on the device)
 
                 .getRecognizer();
         recognizer.addListener(this);
@@ -241,21 +322,17 @@ public class PocketSphinxActivity extends Activity implements
         File menuGrammar = new File(assetsDir, "menu.gram");
         recognizer.addGrammarSearch(MENU_SEARCH, menuGrammar);
 
-        // Create grammar-based search for digit recognition
-        File digitsGrammar = new File(assetsDir, "digits.gram");
-        recognizer.addGrammarSearch(DIGITS_SEARCH, digitsGrammar);
+        // Words recognition
+        File mine_ngram = new File(assetsDir, "mine.gram");
+        recognizer.addGrammarSearch(MINE_GRAM, mine_ngram);
 
-        // Create language model search
-        File languageModel = new File(assetsDir, "weather.dmp");
-        recognizer.addNgramSearch(FORECAST_SEARCH, languageModel);
+        // Continuous speech recognition
+        File continousSpeech = new File(assetsDir, "en-us.lm.bin");
+        // Create keyword-activation search.
+        recognizer.addNgramSearch(CONT_SPEECH, continousSpeech);
+        // Start the search
+        //recognizer.startListening(CONT_SPEECH);
 
-        // Phonetic search
-        File phoneticModel = new File(assetsDir, "en-phone.dmp");
-        recognizer.addAllphoneSearch(PHONE_SEARCH, phoneticModel);
-
-        // My exmpl
-        File continousSpeech = new File(assetsDir, "mine.gram");
-        recognizer.addGrammarSearch(CONT_SPEECH, continousSpeech);
     }
 
     @Override
